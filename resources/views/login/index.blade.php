89b1044269de4c33d5e3395697ccedcd7b2d75b5
khaledfathi <dev@khaledfathi.com>
2023-06-18 19:04:49 +0300

    style login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="{{asset('assets/css/login/login.css')}}">
 </head>
 <body>
-    <div>
+    <div class="container">
         <div>
             @if ($errors->any())
                 <h3>{{$errors->first()}}</h3>
