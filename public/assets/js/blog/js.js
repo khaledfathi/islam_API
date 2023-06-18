9edebb0eb17ca276efc70896a52222b8a44e6096
khaledfathi <dev@khaledfathi.com>
2023-06-19 01:07:12 +0300

    fix time format

--- public/assets/js/blog/js.js
+++ public/assets/js/blog/js.js
@@ -7,8 +7,8 @@ function getCurrentTime(){
     let date = (timeNow.getDate()<10)?"0"+timeNow.getDate():timeNow.getDate();
     let month = (timeNow.getMonth()<10)?"0"+timeNow.getMonth():timeNow.getMonth();
     let year = timeNow.getFullYear();
-    let hours = timeNow.getHours();
-    let minutes = timeNow.getMinutes();
+    let hours = (timeNow.getHours()<10)?"0"+timeNow.getHours():timeNow.getHours();
+    let minutes = (timeNow.getMinutes()<10)?"0"+timeNow.getMinutes():timeNow.getMinutes();
     return `${year}-${month}-${date}T${hours}:${minutes}`; 
 }
 
